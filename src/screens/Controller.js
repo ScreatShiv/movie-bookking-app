6bd9b5afc7dd850328a62b5baa9deffe21180a1d
ScreatShiv <shivamlko96@gmail.com>
2022-01-12 19:56:35 +0530

    first commit

---
+++ src/screens/Controller.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Home from '../screens/home/Home';
+import Details from '../screens/details/Details';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import BookShow from '../screens/bookshow/BookShow'
+ 
+class Controller extends Component {
+
+  constructor() {
+    super();
+    this.baseUrl = "http://127.0.0.1:3309/api/v1/";
+  }
+  render() {
+    return (
+      <Router>
+        <div className="main-container">
+          <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
+          <Route path='/movie/:id' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
+          <Route path='/bookshow/:id' render={(props) => <BookShow {...props} baseUrl={this.baseUrl} />} />
+        </div>
+      </Router>
+
+    )
+  }
+}
+
+export default Controller;
